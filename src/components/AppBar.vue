9332c0cb4afb5cc2e6069dc13d156084973d0c5a
Weezya <nguema@et.esiea.fr>
2021-10-04 02:40:36 +0200

    Commit

---
+++ src/components/AppBar.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <v-app-bar
+      app
+      color="primary"
+      dark
+      shrink-on-scroll
+      src="https://images.unsplash.com/photo-1570876050997-2fdefb00c004?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
+      >
+
+      <v-app-bar-title
+        class="ml-16">
+          TEXTILSHOWROOM
+      </v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+</template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-footer
+    dark
+    padless
+  >
+    <v-col
+      flat
+      tile
+      class="text-center"
+    >
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 my-4 white--text"
+          icon
+        >
+          <v-icon size="24px">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} — TEXTILSHOWROOM
+      </v-card-text>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
+    }),
+  }
+</script>
